eaa533869508d9db3d1bdfca8c446508df90e9f8
TezSmith <Montez.Smith1@gmail.com>
2018-06-23 19:10:14 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
 
     resp = Rack::Response.new
-    time = Time.new
 
-    if time.hour > 0 && time.hour <= 12
+
+    if Time.now.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
